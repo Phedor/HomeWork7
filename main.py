661ec0b8214fd3da95832bf595689455e6c2951f
Phedor <phedor@yandex.ru>
2024-06-18 18:05:49 +0300

    Добавлен модуль processing

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from src.widget import mask_account_card
 from src.widget import format_date
-
+from src.processing import filter_by_state, sort_by_date
 
 def main() -> None:
-    print("Тестирование маскировки и отработки ошибок по номеру счетов и карт")
-    print(f'Visa Platinum 7000 7922 8960 6361 -'
-          f'{(mask_account_card("Visa Platinum 7000 7922 8960 6361") == "Visa Platinum 7000 79** **** 6361")}')
-    print(f'Maestro 7000 7922 8960 6361 - '
-          f'{(mask_account_card("Maestro 7000 7922 8960 6361") == "Maestro 7000 79** **** 6361")}')
-    print(f'Счет 73654108430135874305 - '
-          f'{(mask_account_card("Счет 73654108430135874305") == "Счет **4305")}')
-    print(f'Visa Electron 7000 - '
-          f'{(mask_account_card("Visa Electron 7000") == None)}')
-    print(f'zzZzz - '
-          f'{(mask_account_card("zzZzz") == None)}')
-
-    print("\nПроверка преобразования дат")
-    print(f'"2018-07-11T02:26:18.671407" - '
-          f'{(format_date("2018-07-11T02:26:18.671407") == "11.07.2018")}')
-    print(f'"2018-AA-11T02:26:18.671407" - '
-          f'{(format_date("2018-AA-11T02:26:18.671407"))}')
+    test = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    print(f'Сортировка по убыванию: {sort_by_date(test, asc=False)}')
+    print(f'Сортировка по возрастанию: {sort_by_date(test, asc=True)}')
+    print(f'Фильрация по статусу: {filter_by_state(test)}')
 
 if __name__ == "__main__":
     main()
---
+++ src/processing.py
@@ -0,0 +1,11 @@
+def filter_by_state(processing_list: list):
+    result = []
+    for i in processing_list:
+        if i['state'] == 'EXECUTED':
+            result.append(i)
+    return result
+
+
+def sort_by_date(info: list, asc=True)->list [str]:
+    result = sorted(info, key=lambda x: x['date'], reverse=not asc)
+    return result
