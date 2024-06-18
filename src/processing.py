a4fe6efe4e906254dd3ba9ec1d95cb9a01c91e56
Phedor <phedor@yandex.ru>
2024-06-18 18:21:47 +0300

    Добавлены описания к функциям

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,9 @@
 def filter_by_state(processing_list: list):
+    '''
+    Функция фильтрация одобренных транзакций
+    :param processing_list: список всех транзакций
+    :return: список отобранных транзакций
+    '''
     result = []
     for i in processing_list:
         if i['state'] == 'EXECUTED':
@@ -7,5 +12,11 @@ def filter_by_state(processing_list: list):
 
 
 def sort_by_date(info: list, asc=True) -> list[str]:
+    '''
+    Функция сортировки списка транзакций по дате проведения
+    :param info: список транзакций
+    :param asc: вид сортировки возрастание/убывание
+    :return: отсортированнй список
+    '''
     result = sorted(info, key=lambda x: x['date'], reverse=not asc)
     return result
