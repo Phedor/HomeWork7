25b7ff341727e1ffd93dcc502288382a99c55072
Phedor <phedor@yandex.ru>
2024-06-19 19:24:35 +0300

    Добавлено тестирование масок карты и счета

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,7 @@
 def get_mask_card_number(card_number: str) -> str | None:
     """ Возвращает маску номера карты """
+    if len(card_number) != 16:
+        return None
     while " " in card_number:
         card_number = card_number.replace(" ", "")
     mask = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
@@ -10,5 +12,7 @@ def get_mask_account(account_number: str) -> str | None:
     """ Возвращает маску номера счета
     :type account_number: Номер счета
     """
+    if len(account_number) != 20:
+        return None
     mask = "**" + account_number[-4:]
     return mask
